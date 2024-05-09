9a8cb44e0a279fafacce92d1212e2c1c4eab9527
Sergio Chin-Sue <s.chinsue@gmail.com>
2024-05-09 18:18:46 +0100

    Complete all until errors6.rs

---
+++ move_semantics/move_semantics5.rs
@@ -0,0 +1,17 @@
+// move_semantics5.rs
+//
+// Make me compile only by reordering the lines in `main()`, but without adding,
+// changing or removing any of them.
+//
+// Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
+// for a hint.
+
+#[test]
+fn main() {
+    let mut x = 100;
+    let z = &mut x;
+    *z += 1000;
+    let y = &mut x;
+    *y += 100;
+    assert_eq!(x, 1200);
+}
